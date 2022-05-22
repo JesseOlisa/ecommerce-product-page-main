225480f31b9eaa212ce261bba9f006cc991bf344
JesseOlisa <jessesamuel84@gmail.com>
2022-05-22 16:05:13 +0200

    finishing changes

--- JS/nav.js
+++ JS/nav.js
@@ -6,16 +6,19 @@ const closeNav = document.querySelector('.close-nav');
 hamburger.addEventListener('click', ()=> {
     navContainer.style= ` display: block;`;
     const toOpenNav = ()=> {
-        navLinksContainer.style.transform = 'translateX(0%)';
+        navLinksContainer.classList.add('open-nav');
     }
     const openNav = setTimeout(toOpenNav, 200);
 });
 
 closeNav.addEventListener('click', ()=>{
-    navLinksContainer.style.transform = 'translateX(-100%)';
-    // navContainer.style.display = 'none';
+    navLinksContainer.classList.remove('open-nav');
     const toCloseNav = ()=> {
-        navContainer.style.display = 'none'
+        navContainer.style.display = 'none';
     }
     const closeContainer = setTimeout(toCloseNav, 290)
 });
+
+window.onload = () => {
+    document.body.classList.remove('preload');
+}
